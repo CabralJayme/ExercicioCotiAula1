18a04fa9ace25f0a2796cc73337e6e2fa3fe7bce
jayme <jayme@JAYME-PC>
2024-04-02 23:08:20 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioCotiPOO/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using ExercicioCotiPOO.Entities;
+using ExercicioCotiPOO.Repositories;
+
+Console.WriteLine("CADASTRO DE FUNCIONARIOS: ");
+
+var funcionario = new Funcionario();
+
+funcionario.IdFuncionario = Guid.NewGuid();
+funcionario.DataAdmissao = DateTime.Now;
+
+Console.Write("INFORME O NOME DO FUNCIONARIO:  ");
+funcionario.Name = Console.ReadLine();
+
+Console.Write("INFORME A MATRICULA DO FUNCIONARIO:  ");
+funcionario.Matricula = Console.ReadLine();
+
+Console.Write("INFORME O CPF DO FUNCIONARIO:  ");
+funcionario.Cpf = Console.ReadLine();
+
+Console.Write("INFORME O CARGO DO FUNCIONARIO:  ");
+funcionario.Cargo = Console.ReadLine();
+
+Console.Write("INFORME O SALARIO DO FUNCIONARIO:  ");
+funcionario.Salario = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("##################################################");
+
+var funcionarioRepository = new FuncionarioRepository();
+
+
+funcionarioRepository.ExportaDados(funcionario);
+
+
+// Dados do Funcionario 
+
+Console.WriteLine("DADOS DO FUNCIONARIO: ");
+Console.WriteLine("ID: " + funcionario.IdFuncionario);
+Console.WriteLine("NOME: " + funcionario.Name);
+Console.WriteLine("MATRICULA: " + funcionario.Matricula);
+Console.WriteLine("CPF: " + funcionario.Cpf);
+Console.WriteLine("CARGO: " + funcionario.Cargo);
+Console.WriteLine("SALARIO: " + funcionario.Salario);
+Console.WriteLine("DATA ADMISSAO: " + funcionario.DataAdmissao);
---
+++ ExercicioCotiPOO/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,29 @@
+﻿using ExercicioCotiPOO.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCotiPOO.Repositories
+{
+    public class FuncionarioRepository
+    {
+        public void ExportaDados(Funcionario funcionario)
+        {
+            var streamWriter = new StreamWriter("C:\\Users\\jayme\\OneDrive\\Desktop\\arquivosgerados\\funcionarios.txt", true);
+
+            streamWriter.WriteLine("ID: " + funcionario.IdFuncionario);
+            streamWriter.WriteLine("Name: " + funcionario.Name);
+            streamWriter.WriteLine("Matricula: " + funcionario.Matricula);
+            streamWriter.WriteLine("CPF:" + funcionario.Cpf);
+            streamWriter.WriteLine("Data Admissão: " + funcionario.DataAdmissao);
+            streamWriter.WriteLine("Cargo: " + funcionario.Cargo);
+            streamWriter.WriteLine("Cargo: " + funcionario.Salario);
+
+            streamWriter.WriteLine("...");
+
+            streamWriter.Close();
+        }
+    }
+}
